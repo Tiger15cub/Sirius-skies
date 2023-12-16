df8e2b80e5b25422adeed776797b5385fd1927b1
Skies <118663359+Skiesuwu@users.noreply.github.com>
2023-12-16 14:55:47 -0700

    blurl

---
+++ src/xmpp/types/XmppTypes.ts
@@ -0,0 +1,70 @@
+import { v4 as uuid } from "uuid";
+import WebSocket from "ws";
+
+export interface AccessToken {
+  accountId: string;
+  token: string;
+}
+
+export interface XmppClients extends AccessToken {
+  accountId: string;
+  displayName?: string;
+  jid?: string;
+  token: string;
+  lastPresenceUpdate?: {
+    away: boolean;
+    status: {};
+  };
+  socket: WebSocket;
+}
+
+export interface MUCs {
+  members: [];
+}
+
+export let UUID: string = uuid();
+export let accountId: string = "";
+export let isAuthenticated: boolean = false;
+export let token: string = "";
+export let jid: string = "";
+
+export interface Globals {
+  exchangeCodes: Record<string, any>;
+  clientTokens: any[];
+  AccessTokens: AccessToken[];
+  Clients: XmppClients[];
+  MUCs: Record<string, any>;
+  parties: any[];
+  invites: any[];
+  pings: any[];
+  UUID: string;
+  isAuthenticated: boolean;
+  accountId: string;
+  token: string;
+  jid: string;
+}
+
+export let exchangeCodes: Globals["exchangeCodes"] = {};
+export let clientTokens: Globals["clientTokens"] = [];
+export let AccessTokens: Globals["AccessTokens"] = [];
+export let Clients: Globals["Clients"] = [];
+export let MUCs: Globals["MUCs"] = {};
+export let parties: Globals["parties"] = [];
+export let invites: Globals["invites"] = [];
+export let pings: Globals["pings"] = [];
+
+export const Globals: Globals = {
+  exchangeCodes,
+  clientTokens,
+  AccessTokens,
+  Clients,
+  MUCs,
+  parties,
+  invites,
+  pings,
+  UUID,
+  isAuthenticated,
+  accountId,
+  token,
+  jid,
+};
